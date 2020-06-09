995b20dde01572b3949fefee1793021d04dd26e7
bsylla <babacar.sylla@altran.com>
2020-06-10 00:27:30 +0200

    Replace is expected with should

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Website, type: :model do
 
   context "validations" do
-    it{ is_expected.to validate_presence_of :url }
-    it{ is_expected.to validate_presence_of :scan_schedule }
-    it{ is_expected.to validate_presence_of :weekly_scan_day }
-    it{ is_expected.to validate_presence_of :added_date }
+    it{ should validate_presence_of :url }
+    it{ should validate_presence_of :scan_schedule }
+    it{ should validate_presence_of :weekly_scan_day }
+    it{ should validate_presence_of :added_date }
     it{ should have_many :kookies }
   end
 end
