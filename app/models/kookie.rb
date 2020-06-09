5d01688bb073cebf34eb1cf1874d79c4339c0f0a
bsylla <babacar.sylla@altran.com>
2020-06-09 23:41:55 +0200

    Add cookie test and validation

--- app/models/kookie.rb
+++ app/models/kookie.rb
@@ -1,3 +1,5 @@
 class Kookie < ApplicationRecord
   belongs_to :description
+
+  validates_presence_of :name, :url, :value, :path, :domain, :secure, :httponly, :seen_date, :expiry
 end
--- spec/models/kookie_spec.rb
+++ spec/models/kookie_spec.rb
@@ -1,15 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Kookie, type: :model do
-  subject { Kookie.new(name: "Lorem ipsum",
-                            description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux",
-                            online_description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux")
+  let!(:desc) { Description.new(name: "Lorem ipsum", description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux", online_description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux") }
+  subject {
+    Kookie.new(name: "Cookie 1", url: "test.com/test/path", value: "cookie", path: "/test/path", domain: "test.com", secure: "secure", httponly: "yes", expiry: DateTime.now, seen_date: DateTime.now)
   }
 
   before { subject.save }
 
   it "name should be present" do
     subject.name = nil
-    expect(subject).to_not be_valid
+    expect(subject).not_to be_valid
   end
+  it "url should be present" do
+    subject.url = nil
+    expect(subject).not_to be_valid
+  end
+  it "value should be present" do
+    subject.value = nil
+    expect(subject).not_to be_valid
+  end
+  it "path should be present" do
+    subject.path = nil
+    expect(subject).not_to be_valid
+  end
+  it "domain should be present" do
+    subject.domain = nil
+    expect(subject).not_to be_valid
+  end
+  it "secure should be present" do
+    subject.secure = nil
+    expect(subject).not_to be_valid
+  end
+  it "expiry should be present" do
+    subject.expiry = nil
+    expect(subject).not_to be_valid
+  end
+  it "seen_date should be present" do
+    subject.seen_date = nil
+    expect(subject).not_to be_valid
+  end
+
+  it{should belong_to(:description)}
 end
