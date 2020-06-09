7b01a461c06d6df73e1f09e7bd4faf171cd1f62b
bsylla <babacar.sylla@altran.com>
2020-06-09 23:03:44 +0200

    Add first test to website

--- spec/models/description_spec.rb
+++ spec/models/description_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Description, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe(Description, type: :model) {
+  subject { Description.new(name: "Lorem ipsum",
+                            description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux",
+                            online_description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux")
+  }
+
+  before { subject.save }
+
+  it "name should be present" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+}
