279673365a08a82c0fde835e8f7ff3129a604230
bsylla <babacar.sylla@altran.com>
2020-06-10 10:30:19 +0200

    add enum test to website

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Website, type: :model do
   context "validations" do
     it{ should validate_presence_of :url }
     it{ should validate_presence_of :scan_schedule }
+    it{ should define_enum_for(:scan_schedule)}
     it{ should validate_presence_of :weekly_scan_day }
     it{ should validate_presence_of :added_date }
     it{ should have_many :kookies }
